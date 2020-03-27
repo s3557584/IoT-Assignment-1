9c962d83837844e12da3c1443309615146c0cd80
rmit-s3716022-Jason <42194592+rmit-s3716022-Jason@users.noreply.github.com>
2020-03-28 06:53:53 +1100

    Add files via upload
    Refresh still in progress

---
+++ TaskBMain.py
@@ -0,0 +1,11 @@
+from monitorAndDisplay import monitorAndDisplay
+from sense_hat import SenseHat
+
+sense = SenseHat()
+monitor = monitorAndDisplay(None)
+
+monitor.getColour()
+
+
+
+
---
+++ monitorAndDisplay.py
@@ -0,0 +1,33 @@
+from sense_hat import SenseHat
+from time import sleep
+import json
+
+sense = SenseHat()
+
+
+class monitorAndDisplay:
+    
+    def __init__(self, colour):
+        self.colour = colour
+        
+        
+    def getColour(self):
+        with open('config.json') as config_file:
+            data = json.load(config_file)
+    
+        cold_max = data['cold_max']
+        comfortable_min = data['comfortable_min']
+        comfortable_max = data['comfortable_max']
+        hot_min = data['hot_min']
+        
+        temp = sense.get_temperature()
+
+        
+        while True:
+            if temp < cold_max:
+                sense.clear((0, 0, 255))
+            elif comfortable_min < temp < comfortable_max:
+                sense.clear((0, 255, 0))
+            elif temp > hot_min:
+                sense.show_message(str(temp), text_colour = (255, 0, 0), back_colour = (0 ,0 ,0))
+        time.sleep(10)
